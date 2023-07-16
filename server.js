cda3636f3f854205f9d413b08ad9bb80593bba9a
Vo Kim Thanh <kt121195@gmail.com>
2023-07-16 09:23:27 +0700

    setup vercel 9

--- server.js
+++ server.js
@@ -11,4 +11,4 @@ app.use(function(req, res) {
     res.status(404).send({url: req.originalUrl + ' not found'});
 })
 app.listen(port);
-console.log('START server  api' ,process.env.PORT);
+console.log('START server  api');// ,process.env.PORT);

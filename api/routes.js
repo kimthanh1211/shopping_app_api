4745a0b0bfd69302c22915364c7fd1b3b087103b
Vo Kim Thanh <kt121195@gmail.com>
2023-07-16 11:25:36 +0700

    new update 4

--- api/routes.js
+++ api/routes.js
@@ -1,7 +1,10 @@
 'use strict';
 module.exports = function(app){
-    //let productCtrl = require('./controllers/productsController');
+    //let productCtrl = require('./controllers/ProductsController');
     //to do router
+    app.get('/products',function(req,res){
+        document.write("Test!");
+    });
     //app.get('/products',productCtrl.get);
     //app.get('/product/:productId',productCtrl.detail)
     //app.router('/products')

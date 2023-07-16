802e5e25f38c90bdc4ee89b1dfd266f7b61ae348
Vo Kim Thanh <kt121195@gmail.com>
2023-07-16 10:38:40 +0700

    new update

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ let port=3000
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-let routes = require('./api/routes') //importing route
-routes(app)
 app.use(function(req, res) {
     res.status(404).send({url: req.originalUrl + ' not found'});
 })
+//let routes = require('./api/routes') //importing route
+//routes(app)
 app.listen(port);
 console.log('START server  api');// ,process.env.PORT);

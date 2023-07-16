94d20d139f6de290a82b764d773f50cf7a4fcc50
Vo Kim Thanh <kt121195@gmail.com>
2023-07-16 16:11:20 +0700

    update

--- api/routes.js
+++ api/routes.js
@@ -3,7 +3,11 @@ module.exports = function(app){
     let productCtrl = require('./controllers/products_controller');
     //to do router
     app.get('/products',productCtrl.get);
-    app.get('/product/:productId',productCtrl.detail)
+    app.get('/product/:productId',productCtrl.detail);
+    app.get('/cart',function(){
+
+    });
+
     //app.router('/products')
         //.get(productCtrl.get)
         //.post(productCtrl.store);

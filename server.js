35225d26f71ab32b6cc634229b5f41677f9625f9
Vo Kim Thanh <kt121195@gmail.com>
2023-07-18 11:48:11 +0700

    update add images 4

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(express.static('public'));
 let routes = require('./api/routes') //importing route
-routes(app)
+routes(app);
 app.use(function(req, res) {
     res.status(404).send({url: req.originalUrl + ' not found'});
 })

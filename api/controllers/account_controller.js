c1fddc0f69766a1328b1b8fe50415d0d99d748b5
Vo Kim Thanh <kt121195@gmail.com>
2023-07-18 15:44:21 +0700

    test post request 4

--- api/controllers/account_controller.js
+++ api/controllers/account_controller.js
@@ -14,33 +14,43 @@ module.exports ={
             data:req.body
         }
         res.json(json);
-    },
-    checkExistAccount: (req, res) => {
-        var reqData = req.body
+var reqData = req.body
         async function find() {
           try {
             // Connect the client to the server	(optional starting in v4.7)
             await client.connect();
             const collection = database.collection(collectionName);
+            var json = null;
             try {
                 const findExist = await collection.count({"account_id": req.body.id});
-
-                var json = {
-                    message:"success",
-                    data:{totalAccount: findExist}
+                if(findExist==0){
+                    //Insert account
+                    let id = req.body.id
+                    let password=req.body.password
+                    var dataAccount ={account_id:id,password:password,date_created : Date(),token:""};
+                    const insertResult = await collection.insertOne(dataAccount);
+                    json = {
+                         message:"success",
+                         data:insertResult
+                    }
                 }
-                //res.json(response);
+                else{
+                    json = {
+                        message:"Tên đăng nhập đã tồn tại",
+                        data:""
+                    }
+                }
                 res.json(json);
-              } catch (err) {
+            } catch (err) {
                 console.error(`Something went wrong trying to find the documents: ${err}\n`);
-              }
+            }
           } finally {
             // Ensures that the client will close when you finish/error
             await client.close();
           }
         }
         find().catch(console.dir);
-        //console.log(res)
+
     },
     getInfoByToken: (req, res) => {
         var ObjectId = mongodb.ObjectId;

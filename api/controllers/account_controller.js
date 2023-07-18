c20f80d9e0e08257736db722c2c6e9048bbfaab3
Vo Kim Thanh <kt121195@gmail.com>
2023-07-18 20:16:28 +0700

    post register

--- api/controllers/account_controller.js
+++ api/controllers/account_controller.js
@@ -20,19 +20,25 @@ module.exports ={
                 message:"",
                 data: null
             };
-            let id = req.body.id
+            let email = req.body.email
             let password=req.body.password
-            if(id == undefined || id == null || id ==""){
-                json.message = "Tên đăng nhập không được bỏ trống";
+            let name=req.body.name
+            let phone=req.body.phone
+            let address=req.body.address
+            if(email == undefined || email == null || email ==""){
+                json.message = "Email không được bỏ trống";
             }
             else if(password == undefined || password == null || password ==""){
                 json.message = "Mật khẩu không được bỏ trống";
             }
-            else if(id.length <6 || id.length >24){
-                json.message = "Tên đăng nhập phải từ 6-24 ký tự";
+            else if(name == undefined || name == null || name ==""){
+                json.message = "Họ tên không được bỏ trống";
             }
-            else if(containsSpecialChars(id)){
-                json.message = "Tên đăng nhập không được chứa ký tự đặc biệt";
+            else if(address == undefined || address == null || address ==""){
+                json.message = "Địa chỉ không được bỏ trống";
+            }
+            else if(!validateEmail(email)){
+                json.message = "Email sai định dạng";
             }
             else if(password.length <6 || password.length >24){
                 json.message = "Mật khẩu phải từ 6-24 ký tự";
@@ -44,13 +50,20 @@ module.exports ={
                     const findExist = await collection.count({"account_id": req.body.id});
                     if(findExist==0){
                         //Insert account
-                        let dataAccount ={account_id:id,password:encryption.encryptMd5(password),date_created : Date(),token:encryption.encryptMd5(token)};
+                        let dataAccount ={
+                            email:email
+                            ,password:encryption.encryptMd5(password)
+                            ,name:name
+                            ,phone:phone
+                            ,address:address
+                            ,date_created : Date()
+                            ,token:encryption.encryptMd5(token)};
                         const insertResult = await collection.insertOne(dataAccount);
                         json.message="success",
                         json.data=insertResult
                     }
                     else{
-                        json.message="Tên đăng nhập đã tồn tại";
+                        json.message="Email đăng ký đã tồn tại";
                     }
 
                 } catch (err) {
@@ -98,4 +111,7 @@ module.exports ={
 
 function containsSpecialChars(str) {
   return (/[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/).test(str);
+}
+function validateEmail(str) {
+  return (/^(?=.{1,254}$)(?=.{1,64}@)[-!#$%&'*+/0-9=?A-Z^_`a-z{|}~]+(\.[-!#$%&'*+/0-9=?A-Z^_`a-z{|}~]+)*@[A-Za-z0-9]([A-Za-z0-9-]{0,61}[A-Za-z0-9])?(\.[A-Za-z0-9]([A-Za-z0-9-]{0,61}[A-Za-z0-9])?)*$/).test(str);
 }

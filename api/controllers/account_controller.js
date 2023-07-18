9c4026e3ce33336fb7906500078548e130498bbe
Vo Kim Thanh <kt121195@gmail.com>
2023-07-18 16:09:25 +0700

    test post request 6

--- api/controllers/account_controller.js
+++ api/controllers/account_controller.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const crypto = require('crypto');
+
 const mongodb =require('mongodb');
 const client = require('./../db');
 
@@ -14,30 +16,44 @@ module.exports ={
             // Connect the client to the server	(optional starting in v4.7)
             await client.connect();
             const collection = database.collection(collectionName);
-            var json = null;
-            try {
-                const findExist = await collection.count({"account_id": req.body.id});
-                if(findExist==0){
-                    //Insert account
-                    let id = req.body.id
-                    let password=req.body.password
-                    var dataAccount ={account_id:id,password:password,date_created : Date(),token:""};
-                    const insertResult = await collection.insertOne(dataAccount);
-                    json = {
-                         message:"success",
-                         data:insertResult
-                    }
-                }
-                else{
-                    json = {
-                        message:"Tên đăng nhập đã tồn tại",
-                        data:""
-                    }
-                }
-                res.json(json);
-            } catch (err) {
-                console.error(`Something went wrong trying to find the documents: ${err}\n`);
+            let json = {
+                message:"",
+                data: null
+            };
+            let id = req.body.id
+            let password=req.body.password
+
+            if(id.length <6 || id.length >24){
+                json.message = "Tên đăng nhập phải từ 6-24 ký tự";
             }
+            else if(containsSpecialChars(id)){
+                json.message = "Tên đăng nhập không được chứa ký tự đặc biệt";
+            }
+            else if(password.length <6 || password.length >24){
+                json.message = "Mật khẩu phải từ 6-24 ký tự";
+            }
+            else{
+                try {
+                    //enscrypt md5 password
+                    let pass_md5 = data => crypto.createHash('md5').update(data).digest("hex")
+
+                    const findExist = await collection.count({"account_id": req.body.id});
+                    if(findExist==0){
+                        //Insert account
+                        let dataAccount ={account_id:id,password:pass_md5,date_created : Date(),token:""};
+                        const insertResult = await collection.insertOne(dataAccount);
+                        json.message="success",
+                        json.data=insertResult
+                    }
+                    else{
+                        json.message="Tên đăng nhập đã tồn tại";
+                    }
+
+                } catch (err) {
+                    console.error(`Something went wrong trying to find the documents: ${err}\n`);
+                }
+            }
+            res.json(json);
           } finally {
             // Ensures that the client will close when you finish/error
             await client.close();
@@ -73,4 +89,9 @@ module.exports ={
         find().catch(console.dir);
         //console.log(res)
     },
+}
+
+
+function containsSpecialChars(str) {
+  return (/[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/).test(str);
 }
--- server.js
+++ server.js
@@ -13,3 +13,4 @@ app.use(function(req, res) {
 })
 app.listen(port);
 console.log('START server  api');// ,process.env.PORT);
+

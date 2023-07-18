fc9bc90cec61ddd5c37d83379547c15ea6a54f18
Vo Kim Thanh <kt121195@gmail.com>
2023-07-18 12:12:07 +0700

    test post request

---
+++ api/controllers/account_controller.js
@@ -0,0 +1,88 @@
+'use strict';
+
+const mongodb =require('mongodb');
+const client = require('./../db');
+
+const dbName = "shopping_app";
+const collectionName = "account";
+const database = client.db(dbName);
+
+module.exports ={
+    registerAccount:(req,res)=>{
+        var json = {
+            message:"success",
+            data:req.body
+        }
+        res.json(json);
+    },
+    checkExitst: (req, res) => {
+        let findQuery = { name: { $regex: /Online/ } };
+        async function find() {
+          try {
+            // Connect the client to the server	(optional starting in v4.7)
+            await client.connect();
+            const collection = database.collection(collectionName);
+            try {
+                const cursor = await collection.find(findQuery).sort({ name: -1 });// 1:asc, -1:desc
+                let response=[];
+                await cursor.forEach(result => {
+                  //console.log(result._id.toString())
+                  //console.log(result);
+                  /*var json={
+                    statusCode:1,
+                    statusMessage:'success',
+                    data:result
+                  };
+                  response.push(json)*/
+                  //result.id=result._id.toString();
+                  //result['id'] = result['_id'];
+                  //delete result['_id'];
+
+                  response.push(result);
+                });
+                var json = {
+                    message:"success",
+                    data:response
+                }
+                //res.json(response);
+                res.json(json);
+              } catch (err) {
+                console.error(`Something went wrong trying to find the documents: ${err}\n`);
+              }
+          } finally {
+            // Ensures that the client will close when you finish/error
+            await client.close();
+          }
+        }
+        find().catch(console.dir);
+        //console.log(res)
+    },
+    getInfoBytoken: (req, res) => {
+        var ObjectId = mongodb.ObjectId;
+        let idProduct= new ObjectId(req.params.productId);
+        let findOneQuery = { _id: idProduct };
+        async function find() {
+          try {
+            // Connect the client to the server	(optional starting in v4.7)
+            await client.connect();
+            const collection = database.collection(collectionName);
+            try {
+                let findOneResult = await collection.findOne(findOneQuery);
+                    if (findOneResult === null) {
+                        console.log("Couldn't find any recipes that contain "+idProduct+" as an id.\n");
+                    } else {
+                    console.log(`Found a recipe with "+idProduct+" as an ingredient:\n${JSON.stringify(findOneResult)}\n`);
+                }
+                res.json(findOneResult);
+              } catch (err) {
+                console.error(`Something went wrong trying to find the documents: ${err}\n`);
+              }
+          } finally {
+            // Ensures that the client will close when you finish/error
+            await client.close();
+          }
+        }
+        find().catch(console.dir);
+        //console.log(res)
+    },
+}
--- api/routes.js
+++ api/routes.js
@@ -1,12 +1,15 @@
 'use strict';
 module.exports = function(app){
     let productCtrl = require('./controllers/products_controller');
+    let accountCtrl = require('./controllers/account_controller');
     //to do router
     app.get('/products',productCtrl.get);
     app.get('/product/:productId',productCtrl.detail);
     app.get('/cart',function(req,res){
         res.status(200).send({id: 1,price:10000,products:[]});
     });
+
+    app.post('/account/register',accountCtrl.registerAccount)
 
 
     //app.router('/products')

2bd84de3cab78dafb0515bb3259cd87e0f65f83c
Vo Kim Thanh <kt121195@gmail.com>
2023-10-11 16:06:28 +0700

    update

--- server.js
+++ server.js
@@ -5,13 +5,16 @@ let port=3000
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-app.use(express.static('public'));
-let routes = require('./api/routes') //importing route
+//app.use(express.static('public'));
+const path = require('path');
+app.use('/public', express.static(path.join(__dirname, 'public')));
+let routes = require('./api/routes'); //importing route
 routes(app);
+
 app.use(function(req, res) {
     res.status(404).send({url: req.originalUrl + ' not found'});
 })
-app.listen(port);
+app.listen(port, ()=>console.log((`Server listening on port: ${port}`)));
 console.log('START server  api');// ,process.env.PORT);
 
 const mongodb =require('mongodb');
